5e1bf6a45acc8b804fa371a42673b21ad86883f2
tanergenc <gencdevops@gmail.com>
2021-10-21 16:31:58 +0300

    CreateFolksdevRequest.kt

--- src/main/java/com/example/movie/dto/CreateFolksdevRequest.kt
+++ src/main/java/com/example/movie/dto/CreateFolksdevRequest.kt
@@ -1,9 +1,22 @@
 package com.example.movie.dto
 
+import javax.validation.constraints.*
+
 data class CreateFolksdevRequest(
+        @field:NotNull
         val id: String,
+
+        @field:NotBlank
         val name: String,
-        val age: Int
+
+        @field:NotNull
+        @field:Max(value = 100)
+        @field:Min(value = 1)
+        @field:Positive
+        val age: Int,
+
+        @field:Email
+        val email: String
 )
 
 
---
+++ src/main/java/com/example/movie/exception/GeneralExceptionAdviser.java
@@ -0,0 +1,9 @@
+package com.example.movie.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GeneralExceptionAdviser {
+
+
+}
