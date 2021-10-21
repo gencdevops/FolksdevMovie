fdd6457ad1f8b4786b84e89fbc44d1102fbf8a79
tanergenc <gencdevops@gmail.com>
2021-10-21 17:58:08 +0300

    GeneralExceptionAdviser added

--- src/main/java/com/example/movie/exception/GeneralExceptionAdviser.java
+++ src/main/java/com/example/movie/exception/GeneralExceptionAdviser.java
@@ -1,9 +1,37 @@
 package com.example.movie.exception;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
-public class GeneralExceptionAdviser {
+public class GeneralExceptionAdviser extends ResponseEntityExceptionHandler {
+
+    @NotNull
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  @NotNull HttpHeaders httpHeaders,
+                                                                  @NotNull HttpStatus httpStatus,
+                                                                  @NotNull HttpRequest httpRequest) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+
+
 
 
 }
