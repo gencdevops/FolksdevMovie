9a4ae0de1da692220b5e394fb16bff23679b73da
tanergenc <gencdevops@gmail.com>
2021-11-03 13:25:12 +0300

    Flyway added

--- src/main/java/com/example/movie/repository/ActorRepository.java
+++ src/main/java/com/example/movie/repository/ActorRepository.java
@@ -3,6 +3,8 @@ package com.example.movie.repository;
 import com.example.movie.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ActorRepository extends JpaRepository<Actor, String> {
     List<Actor> findAllByIdIn(List<String> idList);
 
--- src/main/java/com/example/movie/repository/DirectorRepository.java
+++ src/main/java/com/example/movie/repository/DirectorRepository.java
@@ -1,5 +1,6 @@
 package com.example.movie.repository;
 
+import com.example.movie.model.Director;
 import com.example.movie.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
